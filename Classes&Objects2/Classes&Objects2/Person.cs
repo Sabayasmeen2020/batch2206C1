2ce3607206c551e1051ab8f74c501854360a9296
Sabayasmeen2020 <88988257+Sabayasmeen2020@users.noreply.github.com>
2023-09-25 13:02:49 +0500

    25/9/2023

---
+++ Classes&Objects2/Classes&Objects2/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Objects2
+{
+    internal class Person
+    {
+        public string name;
+        public int age;
+
+        public Person() //user defined constructor , no parameter contructor
+        {
+            this.name = "Guest User";
+            this.age = 0;
+        }
+
+        public void eating()
+        {
+            Console.WriteLine(name + " is eating");
+        }
+
+        public void walking()
+        {
+            Console.WriteLine("{0} is walking",name);
+        }
+    }
+}
---
+++ Classes&Objects2/Classes&Objects2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Classes_Objects2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person obj = new Person();
+            
+
+            Console.WriteLine("Person name is : "+obj.name);
+            Console.WriteLine("Person age is : "+obj.age);
+            obj.eating();
+            obj.walking();
+
+            Person obj1 = new Person();
+            obj1.name = "Ahmed";
+            obj1.age = 6;
+
+            Console.WriteLine("Person name is : {0}\nPerson age is : {1}",obj1.name,obj1.age);
+            obj1.eating();
+            obj1.walking();
+
+        }
+    }
+}
