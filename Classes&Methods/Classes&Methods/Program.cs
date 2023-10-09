2f04aad634547c87be5cf42f078e513807bd7634
Sabayasmeen2020 <88988257+Sabayasmeen2020@users.noreply.github.com>
2023-10-09 12:46:09 +0500

    10-9-2023 class work

---
+++ Classes&Methods/Classes&Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Classes_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product objProduct = new Product(1101, "Tooth brush", 54f);
+            //objProduct.productDisplay();
+            //Console.WriteLine(objProduct.getDiscount(5));
+            //double discount =  objProduct.getFlatDiscount();
+            //Console.WriteLine("Flat Discount : " + discount);
+
+            Console.WriteLine(objProduct.add(3, 5));
+            Console.WriteLine(objProduct.add(3.2f));
+            Console.WriteLine(objProduct.add(2));
+            Console.WriteLine(objProduct.add(2.2,4));
+            Console.WriteLine(objProduct.add(2,4.2));
+        }
+    }
+}
---
+++ polymorphism/polymorphism/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class Car : Vehicle
+    {
+        public string model;
+
+        public Car(string model, int speed) : base(speed)
+        {
+            this.model = model;
+        }
+        public void getModel()
+        {
+            Console.WriteLine("Car model is " + model);
+        }
+
+        public void start()
+        {
+            Console.WriteLine("Car is starting at speed" + speed);
+        }
+    }
+}
